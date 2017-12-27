d2df52b0dc05c8a897c16fc0bf25acb2e6807610
eeek77 <34765660+eeek77@users.noreply.github.com>
2017-12-27 13:47:04 +0900

    CharTest3
    good

---
+++ CharTest3.java
@@ -0,0 +1,20 @@
+package Ch3;
+
+public class CharTest3 {
+
+	public static void main(String[] args) {
+	
+		char ch1='h';
+		char ch2='e';
+		char ch3='l';
+		char ch4='l';
+		char ch5='o';
+		
+		System.out.println(ch1+""+ch2+""+ch3+""+ch4+""+ch5);
+		System.out.println(ch1+"\n\n"+ch2+""+ch3+""+ch4+""+ch5);
+		System.out.println(ch1+"\t\t"+ch2+""+ch3+""+ch4+""+ch5);
+		System.out.println(ch1+"\\"+ch2+"\\"+ch3+""+ch4+""+ch5);
+		System.out.println(ch1+"\""+ch2+""+ch3+""+ch4+""+ch5);
+	}
+
+}
